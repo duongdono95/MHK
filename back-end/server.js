e01680af39eb89d6c000b0776cdb36d453fd29a2
duong dono <duong.dno95@gmail.com>
2022-09-07 15:05:05 +1200

    ready file for coding

--- back-end/server.js
+++ back-end/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require ("cors");
 const fs = require("fs");
 const app = express();
-
+const mongoose = mongoose();
 
 app.use(cors());
 app.use(express.json());
