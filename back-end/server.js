3974516ed4c28f3cd801c5901321fd4f51cecbd5
duong dono <duong.dno95@gmail.com>
2022-09-08 13:24:35 +1200

    connect the back-end with mongodb

--- back-end/server.js
+++ back-end/server.js
@@ -2,12 +2,21 @@ const express = require("express");
 const cors = require ("cors");
 const fs = require("fs");
 const app = express();
-const mongoose = mongoose();
+const mongoose = require('mongoose');
+
+
 
 app.use(cors());
 app.use(express.json());
-
-
+// connect to mongodb
+const dbURI = 'mongodb+srv://duongdono:22051995@duongdono.xr4i0nh.mongodb.net/mhk?retryWrites=true&w=majority'
+// connect argument to mongodb
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000, () => { console.log("listening ...")}) )
+    .catch((err) => console.log(err));
+mongoose.connection.on('connected', () => {
+    console.log('successfully connected')
+})
 // get method
 
 
@@ -32,6 +41,3 @@ app.use(express.json());
 
 
 // listen on local host
-app.listen(3000, () => {
-    console.log("listening ...")
-})
