648da61d0c0b049bcf60f7d774916f33d0374692
James Youn <jamesyoundev@gmail.com>
2022-09-09 15:13:55 +1200

    added keyboard styling

--- front-end/src/views/Keyboards.vue
+++ front-end/src/views/Keyboards.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
+    <div class="banner__container"></div>
+    <p class="intro">Current Lisitings</p>
 
+    <button>View</button>
+    <div class="products_list">
+      <div class="product">
+        <div class="image__container">
+          <img class="img" src="http://vision.stanford.edu/aditya86/ImageNetDogs/images/n02106166-Border_collie/n02106166_1663.jpg" alt="product">
+        </div>
+        <p class="product__name">Keyboard 1</p>
+        <p class="seller__name">Duong</p>
+        <p class="price">$119</p>
+        <button class="product__button" type="button">See More -></button>
+    </div>
+    </div>
   </div>
   <Footer />
 </template>
