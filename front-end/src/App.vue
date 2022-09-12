80542839979d4f689d4d064c66d6dcfcf7c6853c
Isha Khopkar <ishakhopkar@ymail.com>
2022-09-12 14:02:31 +1200

    added attempt of delete method

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -33,7 +33,13 @@
 
 <script>
   export default {
-  }
+    const response = await fetch(`http://localhost:3000/keyboardListing/${id}`, {
+          method: 'DELETE',
+        });
+        const data = await response.json();
+        console.log(data);
+        this.keyboardLisitingArray = data;
+      }
 </script>
 
 <style scoped>
