fca96dfe457aaa6056e1f2d8d51bdbb849088846
James Youn <jamesyoundev@gmail.com>
2022-09-12 16:34:19 +1200

    started homepage styling

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-
+    <div class="banner__container">
+      <h2 class="banner__text" >The one stop marketplace for keyboards, mice and headphones</h2>
+    </div>
+    <h2>hello</h2>
+    <h2>hello</h2>
   </div>
   <Footer />
 </template>
@@ -14,5 +18,20 @@
 </script>
 
 <style scoped>
+  *{
+    overflow: hidden;
+  }
+.banner__container {
+  margin-top: 3%;
+  height: 400px;
+  width: 100%;
+  background-image: url(../assets/Keyboard-Banner.png);
+  background-size: cover;
+}
 
+.banner__text{
+  color: white;
+  width: 500px;
+  text-align: center;
+}
 </style>
