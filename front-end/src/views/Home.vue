56683f4d0a1779528efb5b7dfc7c43c239853831
James Youn <jamesyoundev@gmail.com>
2022-09-13 15:26:51 +1200

    edits made

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>
+  <div class="body">
     <div class="banner__container">
-      <h2 class="banner__text" >The one stop marketplace for keyboards, mice and headphones</h2>
+      <h2 class="banner__text" >The one stop marketplace for keyboards, mice and headphones.</h2>
     </div>
-    <h2>hello</h2>
-    <h2>hello</h2>
+    <div class="range__container">
+      <div class="range__title">
+        <h2>Post and Sell or Buy</h2>
+      </div>
+      <div class="range__category">
+        <div class="range__image">
+          <img src="../assets/character/dog-keyboard.png" alt="">
+        </div>
+        <div class="range__text">
+          <div>
+            <ul class="range__list">
+            <li class="range__item"><h1>01</h1></li>
+            <li class="range__item hr"><hr></li>
+            <li class="range__item"><h2>Keyboards</h2></li>
+            <li class="range__item"><strong>Speed. Accuracy. Durability.</strong> Gaming keyboards are designed with the technology and materials required for high performance gaming.</li>
+          </ul>
+          </div>
+          <div>
+            <p>Keyboards</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   <Footer />
 </template>
@@ -18,8 +39,9 @@
 </script>
 
 <style scoped>
-  *{
+  .body{
     overflow: hidden;
+    color: white;
   }
 .banner__container {
   margin-top: 3%;
@@ -30,8 +52,43 @@
 }
 
 .banner__text{
-  color: white;
   width: 500px;
   text-align: center;
+  margin: auto;
+  vertical-align: middle;
+}
+
+.range__title{
+  text-align: center;
+}
+
+.range__image{
+  border: 1px solid white;
+}
+
+.range__image img{
+  width: 400px;
+  height: 400px;
+  object-fit: contain;
+}
+
+.range__category{
+  display: flex;
+}
+
+.range__text{
+  border: 1px solid white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.range__list{
+  list-style: none;
+  padding-left: 0;
+  width: 300px;
+}
+
+.hr{
+  width: 50px;
 }
 </style>
